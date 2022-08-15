0ffe2c0c466db7eda2b82749d274bfe145064a97
Nichlas Handstedt <nichlas.handstedt@scania.com>
2022-08-15 07:43:57 +0200

    refactor

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,8 +6,9 @@ import (
 	"net/http"
 )
 
+// RenderTemplate renders a template to html
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
