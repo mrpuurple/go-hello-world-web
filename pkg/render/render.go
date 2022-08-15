ac313edcba219371d813d5746f47d44898888689
Nichlas Handstedt <nichlas.handstedt@scania.com>
2022-08-15 14:06:07 +0200

    adding template data functionallity

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 
 	"github.com/mrpuurple/go-hello-world-web/pkg/config"
+	"github.com/mrpuurple/go-hello-world-web/pkg/models"
 )
 
 var functions = template.FuncMap{}
@@ -20,7 +21,7 @@ func NewTemplates(a *config.AppConfig) {
 }
 
 // RenderTemplate renders a template to html
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 	if app.UseCache {
 		// get the template cache from the AppConfig
@@ -37,7 +38,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 	buf := new(bytes.Buffer)
 
-	err := t.Execute(buf, nil)
+	err := t.Execute(buf, td)
 	if err != nil {
 		log.Println(err)
 	}
@@ -85,49 +86,3 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 
 	return myCache, nil
 }
-
-// var tc = make(map[string]*template.Template)
-
-// func RenderTemplate(w http.ResponseWriter, t string) {
-// 	var tmpl *template.Template
-// 	var err error
-
-// 	// check to see if we already have the template in our cache
-// 	_, inMap := tc[t]
-// 	if !inMap {
-// 		// we need to create the template
-// 		log.Println("creating template and adding to cache")
-// 		err = createTemplateCache(t)
-// 		if err != nil {
-// 			log.Println(err)
-// 		}
-// 	} else {
-// 		// we have the template in cache
-// 		log.Println("using cached template")
-// 	}
-
-// 	tmpl = tc[t]
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		log.Println(err)
-// 	}
-// }
-
-// func createTemplateCache(t string) error {
-// 	templates := []string{
-// 		fmt.Sprintf("./templates/%s", t),
-// 		"./templates/base.layout.tmpl",
-// 	}
-
-// 	// parse the template
-// 	tmpl, err := template.ParseFiles(templates...)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	// add template to cache (map)
-// 	tc[t] = tmpl
-
-// 	return nil
-// }
